77a7c5f9ffd463f9eb5fd2150cb8a179c2f957b6
Keith Perkins <keith.perkins@cnu.edu>
2023-09-18 15:20:46 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a55_fragments_dynamic/Fragment1.java
@@ -0,0 +1,42 @@
+package com.example.a55_fragments_dynamic;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class Fragment1 extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		//KP DO NOT FORGET THE FALSE
+		return inflater.inflate(R.layout.myfrag1, container,false);	
+	}
+
+	@Override
+	public void onSaveInstanceState(@NonNull Bundle outState) {
+		super.onSaveInstanceState(outState);
+	}
+
+	/**
+	 * Called when the Fragment is no longer resumed.  This is generally
+	 * tied to {@link Activity#onPause() Activity.onPause} of the containing
+	 * Activity's lifecycle.
+	 */
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+	}
+
+	public interface OnFragmentInteractionListener {
+	}
+}
---
+++ app/src/main/java/com/example/a55_fragments_dynamic/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.a55_fragments_dynamic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.os.Bundle;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    final String frag1String = "Fragment1_ID_STRING";
+    boolean showButtons = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // start the fragment manager
+        FragmentManager manager = getSupportFragmentManager();
+
+        // we want a transaction
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.setReorderingAllowed(true);
+
+        transaction.add(R.id.fragment_container1, new Fragment1(), frag1String);
+        transaction.add(R.id.fragment_container2, new Fragment2());
+
+        // either both or neither
+        transaction.commit();
+        manager.executePendingTransactions();
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+
+        // must use fragment manager, get a ref to named fragment (see onCreate())
+        Fragment1 myFrag1 = (Fragment1) getSupportFragmentManager().findFragmentByTag(frag1String);
+
+        // orig transaction could have failed OR fragment UI is not completely created
+        if (myFrag1 != null) {
+            View myView = myFrag1.getView();  //gets root view for fragments layout, the one returned by onCreateView
+            if (myView != null) {
+                TextView myTextView = (TextView) myView.findViewById(R.id.textView1);  //look for views hosted in the root view
+                myTextView.setText("Direct Manipulation");
+
+                //want to set the fragment button handler using an anonymous listener?
+//                Button but=(Button)myView.findViewById(R.id.but1);
+//                but.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        MainActivity.this.doChange2(null);
+//                    }
+//                });
+            }
+        }
+
+    }
+
+    public void doChange2(View view) {
+        showButtons = !showButtons;
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+
+        if (showButtons)
+            transaction.add(R.id.fragment_container2, new Fragment2Radio());
+        else
+            transaction.add(R.id.fragment_container2, new Fragment2());
+
+        transaction.commit();
+        manager.executePendingTransactions();
+    }
+
+}
